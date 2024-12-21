702136e3dc16c34750fa86e100395590f9a66704
Bekarys <abievb91@gmail.com>
2024-12-21 14:40:53 +0500

    Добавлены новые файлы и внесены изменения в проект

--- src/project/Course.java
+++ src/project/Course.java
@@ -1,27 +1,31 @@
 package project;
 
 public class Course {
-	private String courseID;
-	private String name;
-	private CourseType type;
-	
-	public String getCourseID() {
-		return courseID;
-	}
-	
-	public void setCourseID(String courseID) {
-		this.courseID = courseID;
-	}
-	
-	public CourseType getType() {
-		return type;
-	}
+    private String courseID;
+    private String name;
+    private CourseType type;
 
-	public String getName() {
-		return name;
-	}
-	
-	public void setName() {
-		this.name = name;
-	}
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public CourseType getType() {
+        return type;
+    }
+
+    public void setType(CourseType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/project/TechSupporter.java
+++ src/project/TechSupporter.java
@@ -1,16 +1,39 @@
 package project;
 
+import java.util.Vector;
+
 public class TechSupporter extends Employee{
-	private String task;
-	
+	private Vector<String> tasks;
 	
 	public TechSupporter (String id, String name, String position, String email, String task) {
 		super(id, name, position, email);
-		this.task = task;
+		tasks = new Vector<String>();
 	}
 	
-	public void performTask() {
-		System.out.println("Performing task:" + task);
-	}
+	public void acceptOrder(String task) {
+        if (task != null && !task.isEmpty()) {
+            tasks.add(task);  
+            System.out.println("Task accepted: " + task);
+        } else {
+            System.out.println("Invalid task.");
+        }
+    }
 
+    public void rejectOrder(String task) {
+        if (tasks.contains(task)) {
+            tasks.remove(task); 
+            System.out.println("Task rejected: " + task);
+        } else {
+            System.out.println("Task not found: " + task);
+        }
+    }
+
+    public void performTask() {
+        if (!tasks.isEmpty()) {
+            String taskToPerform = tasks.remove(0);  
+            System.out.println("Performing task: " + taskToPerform);
+        } else {
+            System.out.println("No tasks to perform.");
+        }
+    }
 }
